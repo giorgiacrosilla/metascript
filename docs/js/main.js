64db7f38c8d61e7b7bae7214ea1901536a5ac51d
leonardozilli <leonardo.zilli19@gmail.com>
2023-09-15 14:55:56 +0200

    jfks

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,7 +6,7 @@ function carousel() {
   nextButton.addEventListener("click", nextSlide);
   prevButton.addEventListener("click", prevSlide);
   let slideIndex = 0;
-  const slideWidth = slides[0].clientWidth; // Get the width of a single slide
+  const slideWidth = slides[0].clientWidth;
 
   function updateCarousel() {
     carouselTrack.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
@@ -15,7 +15,7 @@ function carousel() {
   function nextSlide() {
     slideIndex++;
     if (slideIndex >= slides.length) {
-      slideIndex = 0; // Wrap around to the first slide
+      slideIndex = 0;
     }
     updateCarousel();
   }
@@ -23,7 +23,7 @@ function carousel() {
   function prevSlide() {
     slideIndex--;
     if (slideIndex < 0) {
-      slideIndex = slides.length - 1; // Wrap around to the last slide
+      slideIndex = slides.length - 1;
     }
     updateCarousel();
   }
@@ -64,54 +64,96 @@ function parallax() {
   });
 }
 
-function sceneSelector() {
-  var xhr = new XMLHttpRequest();
-  xhr.open(
-    "GET",
-    "./src/Arthur Schnitzler - Dream Story (2003, Green Integer).xml",
-    true
-  );
+function populateSelector(zipped_elements) {
+  zipped_elements.forEach(function (element) {
+    var option = document.createElement("option");
+    option.text = element[0].map(item => item.replace('#', ''));
+    option.value = element[1] + ' ' + element[2] + ' ' + element[0];
+    $("#scene-selector").append(option);
+  });
+  getSelectedScene();
+};
+
+function fetchAlignment() {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    var alignmentData = {
+      tElements: [],
+      bElements: [],
+      sElements: [],
+    };
+    xhr.open("GET", "./src/scene-alignment.xml", true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          var xmlDoc = xhr.responseXML;
+          var alignElements = xmlDoc.querySelectorAll(`link`);
+          alignElements.forEach(function (alignElement) {
+            targets = alignElement.getAttribute("target");
+            var parts = targets.split(" ");
+            var tElementsInLink = parts.filter((part) => part.startsWith("#T"));
+            alignmentData.tElements.push(tElementsInLink);
+            var bElementsInLink = parts.filter((part) => part.startsWith("#B"));
+            alignmentData.bElements.push(bElementsInLink);
+            var sElementsInLink = parts.filter((part) => part.startsWith("#S"));
+            alignmentData.sElements.push(sElementsInLink);
+          });
+          resolve(alignmentData);
+        } else {
+          reject(xhr.statusText);
+        }
+      }
+    };
+    xhr.send();
+  });
+}
+
+function fetchAndProcessXML(path, sceneNumbers, container) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", path, true);
+
   xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var xmlDoc = xhr.responseXML;
-      var sceneElements = xmlDoc.querySelectorAll(
-        "div[type='scene']:not([n='none'])"
-      );
-      sceneElements.forEach(function (sceneElement) {
-        var option = document.createElement("option");
-        option.text = sceneElement.getAttribute("n");
-        option.value = sceneElement.getAttribute("n");
-        $("#scene-selector").append(option);
-      });
-      getSelectedScene();
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const xmlDoc = xhr.responseXML;
+
+      if (xmlDoc) {
+        const sceneIds = sceneNumbers.split(',').map(id => id.trim());
+
+        sceneIds.forEach(sceneId => {
+          const element = xmlDoc.querySelector(`div[type="scene"][*|id="${sceneId}"]`);
+          if (element) {
+            const text = element.textContent;
+            $(container).append(text.replace(/\n *\n *\n/gm, "<br>"));
+            console.log(text)
+          } else {
+            console.log(`Scene ${sceneId} not found in ${path}`);
+          }
+        });
+      } else {
+        console.error(`Failed to parse XML from ${path}`);
+      }
     }
   };
+
   xhr.send();
 }
 
-function fetchTextContent(selectedSceneNumber) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(
-    "GET",
-    "./src/Arthur Schnitzler - Dream Story (2003, Green Integer).xml",
-    true
-  );
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var xmlDoc = xhr.responseXML;
-      var scene = xmlDoc.querySelector(
-        `div[type='scene'][n='${selectedSceneNumber}']`
-      );
-      $("#left-text").empty().append(scene);
-    }
-  };
-  xhr.send();
-}
+
+function fetchTextContent(selectedSceneNumbers) {
+    var xmlFiles = [
+    { path: "./src/Arthur Schnitzler - Dream Story (2003, Green Integer).xml", sceneNumber: selectedSceneNumbers[0].replace(/#/g, ''), container: $("#left-text") },
+    { path: "./src/eyes-wide-shut-1996-screenplay.xml", sceneNumber: selectedSceneNumbers[1].replace(/#/g, ''), container: $("#central-text") },
+    { path: "./src/eyes-wide-shut-1999-transcription.xml", sceneNumber: selectedSceneNumbers[2].replace(/#/g, ''), container: $("#right-text") }
+  ];
+  xmlFiles.forEach((item) => {
+    fetchAndProcessXML(item.path, item.sceneNumber, item.container);
+  });
+};
 
 function fetchScreencaps(scenes) {
   var imageContainer = document.getElementById("image-container");
   sceneNumbers = scenes.split(",").map(function (scene) {
-    return scene.trim();
+    return scene.trim().replace("#T", "");
   });
   $("#image-container").empty();
   $.getJSON(
@@ -136,19 +178,27 @@ function fetchScreencaps(scenes) {
 }
 
 function getSelectedScene() {
-  var selectedSceneNumber = $("#scene-selector").val();
+  var selectedSceneNumber = $("#scene-selector").val().split(' ');
   fetchTextContent(selectedSceneNumber);
-  $("#scene-selector").change(function () {
-    selectedSceneNumber = $(this).val();
-    fetchTextContent(selectedSceneNumber);
-    fetchScreencaps(selectedSceneNumber);
-  });
+  fetchScreencaps(selectedSceneNumber[2]);
 }
 
 $(document).ready(function () {
   parallax();
   carousel();
   cardsBackground();
-  fetchScreencaps("4");
-  sceneSelector();
+  fetchAlignment()
+  .then(function (alignmentData) {
+    zippedAlignmentData = alignmentData.tElements.map((tElements, index) => [tElements, alignmentData.bElements[index], alignmentData.sElements[index]]);
+    populateSelector(zippedAlignmentData);
+  })
+  .catch(function (error) {
+    console.error("Error fetching alignment:", error);
+  });
+
+  $("#scene-selector").change(function () {
+    getSelectedScene();
+    $(".comparison-item > pre").empty();
+
+  });
 });
