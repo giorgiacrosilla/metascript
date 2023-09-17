51a7b7f6bb3d7e12b58db8aba73fef2348214fa3
leonardozilli <leonardo.zilli19@gmail.com>
2023-09-17 21:28:12 +0200

    missing screencaps

--- docs/img/screencaps/fold.sh
+++ docs/img/screencaps/fold.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 for filename in ../screencaps_comp/*.png; do
   base_dir="$(dirname "$filename")"
-  filename="$(basename "$filename")"
+  filename="$(basename "$filename" .png)"
   foldname="$(echo "$filename" | cut -d'.' -f1)"
-  if [[ "$filename" == *"SCENE"* ]]; then
-    folder_path="./${foldname}/"
-    if [ ! -d "$folder_path" ]; then
-      echo "Creating folder: $folder_path"
-      mkdir -p "$folder_path" && cp "$base_dir/$filename" "$folder_path"
-    else
-      echo "Folder already exists: $folder_path"
-    fi
+  folder_path="./${foldname}/"
+  if find $folder_path -type f -name "$filename.jpg" -print -quit | grep -q .; then
+    echo "File $filename found."
+  else
+    cp "$base_dir/$filename.png" "$folder_path"
+    echo "File $filename not found in $folder_path"
   fi
 
 done
