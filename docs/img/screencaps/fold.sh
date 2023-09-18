2541ba312ac97dd0d72eaa9d57f912462313c2f8
leonardozilli <leonardo.zilli19@gmail.com>
2023-09-18 15:38:41 +0200

    last screencaps

--- docs/img/screencaps/fold.sh
+++ docs/img/screencaps/fold.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for filename in ../screencaps_comp/*.png; do
+for filename in ../last_screencaps/*.png; do
   base_dir="$(dirname "$filename")"
   filename="$(basename "$filename" .png)"
   foldname="$(echo "$filename" | cut -d'.' -f1)"
@@ -7,6 +7,7 @@ for filename in ../screencaps_comp/*.png; do
   if find $folder_path -type f -name "$filename.jpg" -print -quit | grep -q .; then
     echo "File $filename found."
   else
+    mkdir "$folder_path"
     cp "$base_dir/$filename.png" "$folder_path"
     echo "File $filename not found in $folder_path"
   fi
